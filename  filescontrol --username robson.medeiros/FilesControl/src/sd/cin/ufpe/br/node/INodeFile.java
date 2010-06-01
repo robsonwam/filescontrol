c6e0bf1a0417f5ebf2ecddccaad59a9c87d8362a
salvinhu <salvinhu@6bd01bb5-b82e-b273-671a-7308eb2b223d>
2010-06-01 12:51:04 +0000

    teste

---  filescontrol --username robson.medeiros/FilesControl/src/sd/cin/ufpe/br/node/INodeFile.java
+++  filescontrol --username robson.medeiros/FilesControl/src/sd/cin/ufpe/br/node/INodeFile.java
@@ -3,7 +3,7 @@ package sd.cin.ufpe.br.node;
 import java.io.ObjectInput;
 
 public interface INodeFile {
-	
+	//teste
 	public boolean addChunk(int id, ObjectInput file);
 	public ObjectInput getChunk(int id);
 	public boolean delChunk(int id);
