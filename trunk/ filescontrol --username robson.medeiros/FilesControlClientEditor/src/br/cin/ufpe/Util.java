45de247f389077d8e79307f99082660f45758de0
robson.medeiros <robson.medeiros@6bd01bb5-b82e-b273-671a-7308eb2b223d>
2010-06-24 22:23:49 +0000

    Initial import.

---
+++ trunk/ filescontrol --username robson.medeiros/FilesControlClientEditor/src/br/cin/ufpe/Util.java
@@ -0,0 +1,63 @@
+package br.cin.ufpe;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Util {
+
+	// Returns the contents of the file in a byte array.
+	public static byte[] getBytesFromFile(File file) throws IOException {
+		InputStream is = new FileInputStream(file);
+
+		// Get the size of the file
+		long length = file.length();
+
+		// You cannot create an array using a long type.
+		// It needs to be an int type.
+		// Before converting to an int type, check
+		// to ensure that file is not larger than Integer.MAX_VALUE.
+		if (length > Integer.MAX_VALUE) {
+			// File is too large
+		}
+
+		// Create the byte array to hold the data
+		byte[] bytes = new byte[(int) length];
+
+		// Read in the bytes
+		int offset = 0;
+		int numRead = 0;
+		while (offset < bytes.length
+				&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+			offset += numRead;
+		}
+
+		// Ensure all the bytes have been read in
+		if (offset < bytes.length) {
+			throw new IOException("Could not completely read file "
+					+ file.getName());
+		}
+
+		// Close the input stream and return bytes
+		is.close();
+		return bytes;
+	}
+
+	public static File getFileFromBytes(byte[] bytes) throws IOException {
+		
+		File arquivo = File.createTempFile("Temp"
+				+ (int) (1 + Math.random() * 1000), ".tmp");
+		
+		arquivo.deleteOnExit();
+
+		OutputStream out = new FileOutputStream(arquivo);
+		out.write(bytes);
+		out.close();
+		
+		return arquivo;
+	}
+
+}
